4755d5bb24d8dc73d79d7521f344e436727bd2a9
Sam Hogarth <dev@samhogy.co.uk>
2014-09-13 11:31:50 +0100

    Unit testing the view model!
    Note the use of Execute vs ExecuteAsync. We can use Execute if we just
    want the command to run. But ExecuteAsync will allow us to subscribe to
    the command executing and observe the status of things _as the command
    is running_. That's pretty awesome.

--- Test/FlickrSearchViewModelTests.cs
+++ Test/FlickrSearchViewModelTests.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
-using ReactiveFlickr;
+using Splat;
+using System;
+using System.Reactive.Linq;
 
 namespace ReactiveFlickr.Test
 {
@@ -14,7 +15,8 @@ namespace ReactiveFlickr.Test
             [TestMethod]
             public void Search_CanExecute_Is_False()
             {
-                var vm = new FlickrSearchViewModel(Substitute.For<IImageService>());
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>());
 
                 Assert.IsFalse(vm.Search.CanExecute(null));
             }
@@ -22,7 +24,8 @@ namespace ReactiveFlickr.Test
             [TestMethod]
             public void IsLoading_Is_False()
             {
-                var vm = new FlickrSearchViewModel(Substitute.For<IImageService>());
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>());
 
                 Assert.IsFalse(vm.IsLoading);
             }
@@ -30,9 +33,19 @@ namespace ReactiveFlickr.Test
             [TestMethod]
             public void ShowError_Is_False()
             {
-                var vm = new FlickrSearchViewModel(Substitute.For<IImageService>());
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>());
 
                 Assert.IsFalse(vm.ShowError);
+            }
+
+            [TestMethod]
+            public void CanEnterSearchText_CanExecute_Is_True()
+            {
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>());
+
+                Assert.IsTrue(vm.CanEnterSearchText);
             }
         }
 
@@ -42,12 +55,249 @@ namespace ReactiveFlickr.Test
             [TestMethod]
             public void Search_CanExecute_Is_True()
             {
-                var vm = new FlickrSearchViewModel(Substitute.For<IImageService>())
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>())
                 {
                     SearchText = "testing"
                 };
 
                 Assert.IsTrue(vm.Search.CanExecute(null));
+            }
+
+            [TestMethod]
+            public void IsLoading_Is_False()
+            {
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>())
+                {
+                    SearchText = "testing"
+                };
+
+                Assert.IsFalse(vm.IsLoading);
+            }
+
+            [TestMethod]
+            public void ShowError_Is_False()
+            {
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>())
+                {
+                    SearchText = "testing"
+                };
+
+                Assert.IsFalse(vm.ShowError);
+            }
+
+            [TestMethod]
+            public void CanEnterSearchText_Is_True()
+            {
+                var vm = new FlickrSearchViewModel(
+                    Substitute.For<IImageService>())
+                {
+                    SearchText = "testing"
+                };
+
+                Assert.IsTrue(vm.CanEnterSearchText);
+            }
+        }
+
+        [TestClass]
+        public class When_Searching
+        {
+            FlickrSearchViewModel subject;
+            IImageService service;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                service = Substitute.For<IImageService>();
+                service.GetImages(Arg.Any<string>())
+                    .Returns(Observable.Never<SearchResultViewModel>());
+
+                subject = new FlickrSearchViewModel(service);
+            }
+
+            [TestMethod]
+            public void Search_Executed_With_SearchText()
+            {
+                subject.SearchText = "cats";
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ =>
+                    {
+                        service.Received().GetImages("cats");
+                    });
+            }
+
+            [TestMethod]
+            public void Search_CanExecute_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ =>
+                    {
+                        Assert.IsFalse(subject.Search.CanExecute(null));
+                    });
+            }
+
+            [TestMethod]
+            public void IsLoading_Is_True()
+            {
+                subject.SearchText = "cats";
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ =>
+                    {
+                        Assert.IsTrue(subject.IsLoading);
+                    });
+            }
+
+            [TestMethod]
+            public void ShowError_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ =>
+                    {
+                        Assert.IsFalse(subject.ShowError);
+                    });
+            }
+
+            [TestMethod]
+            public void CanEnterSearchText_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ => 
+                    {
+                        Assert.IsFalse(subject.CanEnterSearchText);
+                    });
+            }
+
+            [TestMethod]
+            public void Images_Is_Empty()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                subject.Search.ExecuteAsync(null)
+                    .Subscribe(_ =>
+                    {
+                        Assert.AreEqual(0, subject.Images.Count);
+                    });
+            }
+        }
+
+        [TestClass]
+        public class When_Search_Succeeds
+        {
+            SearchResultViewModel photo1;
+            SearchResultViewModel photo2;
+            FlickrSearchViewModel subject;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                photo1 = new SearchResultViewModel(
+                    Substitute.For<IBitmap>(), "photo1");
+                photo2 = new SearchResultViewModel(
+                    Substitute.For<IBitmap>(), "photo2");
+
+                var service = Substitute.For<IImageService>();
+                service.GetImages(Arg.Any<string>())
+                    .Returns(
+                        Observable.Create<SearchResultViewModel>(async obs =>
+                        {
+                            obs.OnNext(photo1);
+                            obs.OnNext(photo2);
+                            obs.OnCompleted();
+                        }));
+
+                subject = new FlickrSearchViewModel(service)
+                {
+                    SearchText = "cats"
+                };
+            }
+
+            [TestMethod]
+            public void Images_Contains_Results()
+            {
+                subject.Search.Execute(null);
+
+                Assert.AreEqual(photo1, subject.Images[0]);
+                Assert.AreEqual(photo2, subject.Images[1]);
+            }
+
+            [TestMethod]
+            public void ShowError_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsFalse(subject.ShowError);
+            }
+
+            [TestMethod]
+            public void IsLoading_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsFalse(subject.IsLoading);
+            }
+
+            [TestMethod]
+            public void CanEnterSearchText_Is_True()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsTrue(subject.CanEnterSearchText);
+            }
+        }
+
+        [TestClass]
+        public class When_Search_Fails
+        {
+            FlickrSearchViewModel subject;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                var service = Substitute.For<IImageService>();
+                service.GetImages(Arg.Any<string>())
+                    .Returns(x => 
+                        {
+                            return Observable.Throw<SearchResultViewModel>(new Exception());
+                        });
+
+                subject = new FlickrSearchViewModel(service);
+            }
+
+            [TestMethod]
+            public void ShowError_Is_True()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsTrue(subject.ShowError);
+            }
+
+            [TestMethod]
+            public void IsLoading_Is_False()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsFalse(subject.IsLoading);
+            }
+
+            [TestMethod]
+            public void CanEnterSearchText_Is_True()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.IsTrue(subject.CanEnterSearchText);
+            }
+
+            [TestMethod]
+            public void Images_Is_Empty()
+            {
+                subject.SearchText = "cats";
+                subject.Search.Execute(null);
+                Assert.AreEqual(0, subject.Images.Count);
             }
         }
     }
