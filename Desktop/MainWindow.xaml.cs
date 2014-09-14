34887a4c62ffb874646ab18d38bef36c60b30d06
Paul Betts <paul@paulbetts.org>
2014-09-14 13:57:56 -0700

    Make MainWindow an IViewFor so we can use Bindings

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -1,17 +1,31 @@
 ﻿using System.Windows;
+using ReactiveUI;
 
 namespace ReactiveFlickr.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<FlickrSearchViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+
             var service = new FlickrImageService();
-            DataContext = new FlickrSearchViewModel(service);
+            ViewModel = new FlickrSearchViewModel(service);
+        }
+
+        public FlickrSearchViewModel ViewModel {
+            get { return (FlickrSearchViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(FlickrSearchViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (FlickrSearchViewModel)value; }
         }
     }
 }
